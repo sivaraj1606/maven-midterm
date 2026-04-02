55cff9dcdf88f7f3a145118d6edc8eb26369b908
sivaraj1606 <sivasubramaniyan.r2023@vitstudent.ac.in>
2026-04-02 14:19:16 +0530

    Create AppTest.java

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,26 @@
+package com.numberutil;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class AppTest {
+    
+    App app = new App();
+
+    @Test
+    public void testEvenOdd() {
+        assertEquals("Even", app.checkEvenOdd(10));
+        assertEquals("Odd", app.checkEvenOdd(7));
+    }
+
+    @Test
+    public void testSumOfDigits() {
+        assertEquals(15, app.sumOfDigits(12345));
+    }
+
+    @Test
+    public void testReverseNumber() {
+        assertEquals(54321, app.reverseNumber(12345));
+        assertEquals(-321, app.reverseNumber(-123));
+    }
+}
